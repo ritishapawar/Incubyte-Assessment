c1f98f9fa47aaad258bcabaf77b3196865051e98
ritishapawar <pawar.ritisha08@gmail.com>
2025-04-28 20:25:55 +0530

    Added codr for delimiter in the input given

--- Incubyte-Assessment/stringCalculator.js
+++ Incubyte-Assessment/stringCalculator.js
@@ -1,7 +1,17 @@
 function add(numbers) {
+    // when input is empty string
     if (numbers === '') return 0;
-    const normalizedNumbers = numbers.replace(/\n/g, ',');
-    return normalizedNumbers.split(',').map(Number).reduce((a, b) => a + b, 0);
+
+    if (numbers.startsWith('//')) { // when input starts with delimiter
+      const delimiter = numbers[2]; 
+      numbers = numbers.slice(4);  
+      numbers = numbers.replace(new RegExp(delimiter, 'g'), ',');
+    }
+  
+    numbers = numbers.replace(/[\n,]/g, ','); //It will replace new lines and commas
+  
+    return numbers.split(',').map(Number).reduce((a, b) => a + b, 0);
   }
   
-  module.exports = { add };
+  
+module.exports = { add };
--- Incubyte-Assessment/stringCalculator.test.js
+++ Incubyte-Assessment/stringCalculator.test.js
@@ -16,4 +16,8 @@ describe('String Calculator', () => {
   test('handles new lines between numbers', () => {
     expect(add('1\n2,3')).toBe(6);
   });
+
+  test('supports custom delimiters', () => {
+    expect(add('//;\n1;2')).toBe(3);
+  });
 });
