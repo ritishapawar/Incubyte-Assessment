c9f15c74ae4ba451f753ec44c964cbd8cebea5df
ritishapawar <pawar.ritisha08@gmail.com>
2025-04-28 20:28:12 +0530

    Negative number handled

--- Incubyte-Assessment/stringCalculator.js
+++ Incubyte-Assessment/stringCalculator.js
@@ -2,6 +2,12 @@ function add(numbers) {
     // when input is empty string
     if (numbers === '') return 0;
 
+    // This will handle negative numbers if gievn in input and throws an error
+    const negativeNumbers = numbers.match(/-\d+/g);
+    if (negativeNumbers) {
+      throw new Error(`negative numbers not allowed ${negativeNumbers.join(', ')}`);
+    }
+
     if (numbers.startsWith('//')) { // when input starts with delimiter
       const delimiter = numbers[2]; 
       numbers = numbers.slice(4);  
@@ -12,6 +18,5 @@ function add(numbers) {
   
     return numbers.split(',').map(Number).reduce((a, b) => a + b, 0);
   }
-  
-  
+
 module.exports = { add };
--- Incubyte-Assessment/stringCalculator.test.js
+++ Incubyte-Assessment/stringCalculator.test.js
@@ -20,4 +20,8 @@ describe('String Calculator', () => {
   test('supports custom delimiters', () => {
     expect(add('//;\n1;2')).toBe(3);
   });
+
+  test('throws an exception for negative numbers', () => {
+    expect(() => add('1,-2,3')).toThrow('negative numbers not allowed -2');
+  });
 });
