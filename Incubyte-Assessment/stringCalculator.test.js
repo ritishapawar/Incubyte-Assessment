a0a005c7f7d49616e9feaf0a3caa78d97607b88f
ritishapawar <pawar.ritisha08@gmail.com>
2025-04-28 20:17:13 +0530

    Updated the add method to handle newlines between numbers

--- Incubyte-Assessment/stringCalculator.test.js
+++ Incubyte-Assessment/stringCalculator.test.js
@@ -4,4 +4,16 @@ describe('String Calculator', () => {
   test('returns 0 for an empty string', () => {
     expect(add('')).toBe(0);
   });
+
+  test('returns the number itself when one number is given', () => {
+    expect(add('1')).toBe(1);
+  });
+
+  test('returns the sum of comma-separated numbers', () => {
+    expect(add('1,5')).toBe(6);
+  });
+
+  test('handles new lines between numbers', () => {
+    expect(add('1\n2,3')).toBe(6);
+  });
 });
